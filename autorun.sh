a8ff464ac9fd1b1f2775f30e3337043487c14bf4
ywz978020607 <ywzsunny2012@163.com>
2023-01-25 01:48:53 +0800

    up

--- autorun.sh
+++ autorun.sh
@@ -1,1 +1,2 @@
+# 设置定时: contab: >> bash /data/data/com.termux/files/home/ywz978020607/autorun.sh
 cd /data/data/com.termux/files/home/ywz978020607/ && python3 get_addr_push.py && git add -A && git commit -m 'up' && git push origin main
