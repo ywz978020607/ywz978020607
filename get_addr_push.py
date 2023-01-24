57ec40b43349bd380ffc74b54ad3aa29fe5dc79b
Sharklet <ywzsunny2012@163.com>
2023-01-24 17:20:03 +0000

    up-dns

---
+++ get_addr_push.py
@@ -0,0 +1,16 @@
+# for termux
+import os, json
+
+info = os.popen('ip addr').read()
+ipv6_addr = info.split('wlan0')[-1].split('scope global dynamic mngtmpaddr stable-privacy')[0].split('inet6')[-1].split('/')[0].strip()
+ipv4_inner_addr = info.split('scope global wlan0')[0].split('inet')[-1].split('/')[0].strip()
+
+data = {}
+data['ywz3'] = ipv6_addr
+data['ywz3in'] = ipv4_inner_addr
+
+with open('myip.json', 'w') as f:
+    json.dump(data, f)
+
+print(data)
+print('done')
