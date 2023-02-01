0dbf6f57c7d665958bb6e33ba56fd920f81ca06b
ywz978020607 <ywzsunny2012@163.com>
2023-02-01 08:37:38 +0800

    up

--- get_addr_push.py
+++ get_addr_push.py
@@ -3,7 +3,7 @@ import os, json
 
 info = os.popen('ip addr').read()
 ipv6_addr = info.split('wlan0')[-1].split('scope global temporary dynamic')[0].split('inet6')[-1].split('/')[0].strip()
-ipv4_inner_addr = info.split('scope global wlan0')[0].split('inet')[-1].split('/')[0].strip()
+#ipv4_inner_addr = info.split('scope global wlan0')[0].split('inet')[-1].split('/')[0].strip()
 
 data = {}
 data['ywz3'] = ipv6_addr
