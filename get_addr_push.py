da9e25da929e51c0f4ffc1650e8dde58ee6fc713
ywz978020607 <ywzsunny2012@163.com>
2023-03-04 10:54:17 +0800

    up

--- get_addr_push.py
+++ get_addr_push.py
@@ -1,17 +1,18 @@
 # for termux
-import os, json
+import os, json, sys
 
 info = os.popen('ip addr').read()
 ipv6_addr = info.split('wlan0')[-1].split('scope global temporary dynamic')[0].split('inet6')[-1].split('/')[0].strip()
 ipv4_inner_addr = info.split('scope global wlan0')[0].split('inet')[-1].split('/')[0].strip()
 
 data = {}
-data['ywz3'] = ipv6_addr
-data['ywz3in'] = ipv4_inner_addr
+prefix = str(sys.argv[1])
+data[prefix] = ipv6_addr
+#data['ywz3in'] = ipv4_inner_addr
 
 with open('myip.json', 'w') as f:
     json.dump(data, f)
 
 print(data)
 print('done')
-os.system("timeout 5s ping6 ywz3.buaamc2.net")
+os.system("timeout 3s ping6 {}.buaamc2.net".format(prefix))
