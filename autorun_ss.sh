8f951c1dfe5907add9318a0285d29d546a66c333
ywzsunny <ywzsunny@buaa.edu.cn>
2023-03-04 10:56:20 +0800

    up

---
+++ autorun_ss.sh
@@ -0,0 +1,2 @@
+# 设置定时: contab: >> bash /data/data/com.termux/files/home/ywz978020607/autorun.sh
+cd /root/ywz978020607/ && git pull origin main && python3 get_addr_push.py ywzss && git add -A && git commit -m 'up' && git push origin main
