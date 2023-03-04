180ba7155fa368a88cf2a5a5b0a54184f9519c9f
ywzsunny <ywzsunny@buaa.edu.cn>
2023-03-04 11:12:36 +0800

    up

--- get_addr_push.py
+++ get_addr_push.py
@@ -2,7 +2,7 @@
 import os, json, sys
 
 info = os.popen('ip addr').read()
-ipv6_addr = info.split('wlan0')[-1].split('scope global')[0].split('inet6')[-1].split('/')[0].strip()
+ipv6_addr = info.split('wlan0')[-1].split('scope global')[-2].split('inet6')[-1].split('/')[0].strip()
 #ipv4_inner_addr = info.split('scope global wlan0')[0].split('inet')[-1].split('/')[0].strip()
 
 data = {}
