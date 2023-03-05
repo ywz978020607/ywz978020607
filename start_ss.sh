cf2e866ed7665e6f19824e2e57b401d3eabaded5
ywzsunny <ywzsunny@buaa.edu.cn>
2023-03-05 21:09:53 +0800

    up

---
+++ start_ss.sh
@@ -0,0 +1,7 @@
+cd /root/docker_nginx_with_manage/docker
+docker-compose up -d
+cd /root/server_dockers/nextcloud
+docker-compose up -d
+cd /root/server_dockers/
+bash wangxin.sh
+# emby, syncthing are services
