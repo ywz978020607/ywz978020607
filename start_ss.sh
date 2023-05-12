be6ad8b9cec7376721d47969372d1e25c102fac4
ywzsunny <ywzsunny@buaa.edu.cn>
2023-05-13 01:10:09 +0800

    up

--- start_ss.sh
+++ start_ss.sh
@@ -1,5 +1,7 @@
 cd /root/docker_nginx_with_manage/docker
 docker-compose up -d
+cd /root/server_dockers/frpc
+./frpc_linux_amd64 -c frpc.ini &
 cd /root/server_dockers/nextcloud
 docker-compose up -d
 cd /root/server_dockers/
--- start_termux.sh
+++ start_termux.sh
@@ -1,8 +1,6 @@
 nginx
 cd docker_nginx_with_manage/
 uwsgi --ini django1/uwsgi.ini
-cd /root/server_dockers/frpc
-./frpc_linux_amd64 -c frpc.ini &
 crond start
 sshd
 cd /data/data/com.termux/files/home/syncthing-linux-arm64-v1.23.1/
