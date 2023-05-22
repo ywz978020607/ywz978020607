638ae169a5f233deba02ea97e37e52a634f2bcfe
ywzsunny <ywzsunny@buaa.edu.cn>
2023-05-22 22:45:09 +0800

    up

--- start_ss.sh
+++ start_ss.sh
@@ -1,3 +1,4 @@
+# docker run -d --name ddns-go --restart=always --net=host -v /opt/ddns-go:/root jeessy/ddns-go
 cd /root/docker_nginx_with_manage/docker
 docker-compose up -d
 cd /root/server_dockers/frpc
